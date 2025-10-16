d816a2030e69d9826743411ef8ad3eeaabba32b8
alex5169 <speedshorts89@gmail.com>
2025-10-16 17:44:48 +0800

    blacky

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="text-cat-brown-light text-sm flex flex-col items-center justify-center">
           <p className="font-bold text-base">
-            © 2024 Michi Community. All rights reserved.
+            © 2025 Blacky Community. All rights reserved.
           </p>
           <p className="mt-2 font-medium text-center text-xs max-w-md">
             DISCLAIMER: all Memecoins are high-risk speculative assets with no
