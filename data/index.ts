523159bdb68946664c639dab166af443ebadd851
alex5169 <speedshorts89@gmail.com>
2025-10-16 16:21:22 +0800

    jpg change korsi

--- data/index.ts
+++ data/index.ts
@@ -6,7 +6,7 @@ export const SHORT_DESCRIPTION = `the most memeable cat on the internet`;
 export const MAIN_IMAGE = "/blacky.jpeg";
 
 // CA
-export const CONTRACT_ADDRESS = "5mbK36SZ7J19An8jFochhQS4of8g6BwUjbeCSxBSoWdp";
+export const CONTRACT_ADDRESS = "comming";
 export const CONTRACT_ADDRESS_HREF = `https://raydium.io/swap/?outputCurrency=${CONTRACT_ADDRESS}`;
 
 // Social
@@ -20,15 +20,15 @@ export const MEME_HEADER = `${NAME} memes`;
 export const MEME_DESCRIPTION = `The infinite meme collection - "there's always a ${NAME} for that."`;
 
 export const MEMES = [
-  { src: "/", alt: " vaping meme" },
-  { src: "/", alt: " anime Zoro meme" },
-  { src: "/", alt: " sunglasses meme" },
-  { src: "/", alt: " business meme" },
-  { src: "/", alt: " chef meme" },
-  { src: "/", alt: " superhero meme" },
-  { src: "/", alt: " superhero meme" },
-  { src: "/", alt: " superhero meme" },
-  { src: "/", alt: " superhero meme" },
+  { src: "/blacky1.jpg", alt: " vaping meme" },
+  { src: "/blacky2.jpg", alt: " anime Zoro meme" },
+  { src: "/blacky3.jpg", alt: " sunglasses meme" },
+  { src: "/blacky4.jpg", alt: " business meme" },
+  { src: "/blacky5.jpg", alt: " chef meme" },
+  { src: "/blacky6.jpg", alt: " superhero meme" },
+  { src: "/blacky7.jpg", alt: " superhero meme" },
+  { src: "/blacky8.jpg", alt: " superhero meme" },
+  { src: "/blacky9.jpg", alt: " superhero meme" },
 ];
 
 // Page metadata
@@ -40,11 +40,11 @@ export const META_FAV_ICON = "/blacky.jpeg";
 // Carousel
 export const CAROUSEL_IMAGES = [
   "/blacky.jpeg",
-  "/blacky.jpeg",
-  "/blacky.jpeg",
-  "/blacky.jpeg",
-  "/blacky.jpeg",
-  "/blacky.jpeg",
+  "/blacky2.jpg",
+  "/blacky3.jpg",
+  "/blacky4.jpg",
+  "/blacky5.jpg",
+  "/blacky6.jpg",
 ];
 export const CAROUSEL_DOTS = true;
 export const CAROUSEL_SLIDE_INTERVAL = 2000;
